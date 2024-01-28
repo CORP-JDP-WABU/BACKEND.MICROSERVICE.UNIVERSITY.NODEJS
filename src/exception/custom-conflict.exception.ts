842f35fc5384c8777d5508a9653179ac2b90159b
tismart-fernando <tismart.fernando@gmail.com>
2024-01-27 23:50:40 -0500

    feat: return code unahtorized to university

--- src/exception/custom-conflict.exception.ts
+++ src/exception/custom-conflict.exception.ts
@@ -2,6 +2,6 @@ import { ConflictException } from '@nestjs/common';
 
 export class UnahutorizedUniversityCustomException extends ConflictException {
   constructor(customCode: string) {
-    super(`Usted no esta authorizado para realizar consultas en esta universidad [${customCode}]`);
+    super(`Usted no esta authorizado para realizar consultas en esta universidad [${customCode}`);
   }
 }
